851d48f4699ae9340af7c2ee587668f9d8beb6e2
yulyaaka <ykeldybay@gmail.com>
2025-02-02 11:07:43 +0000

    работа с прелоадером

--- script/preloader.js
+++ script/preloader.js
@@ -1,1 +1,15 @@
-document.addEventListener(type)
+document.addEventListener('DOMContentLoaded', () => {
+    // Функция для плавного удаления прелоадера
+    function removePreloader() {
+        const preloader = document.querySelector('.preloader');
+        preloader.classList.add('hide-preloader');
+        
+        // Полностью удаляем прелоадер через 500мс после начала анимации
+        setTimeout(() => {
+            preloader.remove();
+        }, 500);
+    }
+
+    // Вызываем функцию с небольшой задержкой
+    setTimeout(removePreloader, 500);
+});
